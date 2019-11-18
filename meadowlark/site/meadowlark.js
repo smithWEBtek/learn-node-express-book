fe7824025f3f1a6f1d87cff960ba23a38ea06938
smithwebtek <brad@smithwebtek.com>
2019-11-17 19:40:13 -0500

    Add express server

--- meadowlark/site/meadowlark.js
+++ meadowlark/site/meadowlark.js
@@ -1,25 +1,45 @@
 require('dotenv').config()
-
 var express = require('express');
-
 var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+  .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-// custom 404 page
-app.use (function(req, res){
-  res.type('text/plain');
-  res.status(404);
-  res.send('404 - Not Found');
+app.get('/', function(req, res){
+  res.render('home');
 });
 
-// custom 500 page
-app.use(function(err, req, next){
-  console.error("err.stack: ", err.stack);
-  res.type('text/plain');
-  res.status(500);
-  res.send('500 - Server Error');
+app.use(express.static('public'));
+
+app.get('/about', function(req, res){
+  var fortunes = [
+    "Conquer your fears or they will conquer you.", "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
+    ];
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: randomFortune });
 });
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+  res.status(404);
+  res.render('404');
+});
+ 
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function(){
   console.log('hey meadowlark')
