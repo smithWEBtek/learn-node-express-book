c90bbbd0eed0da50078fd52c12cc164d0a782af6
Brad Smith <brad@smithwebtek.com>
2019-12-02 08:52:26 -0500

    Update meadowlark.js

--- meadowlark/site/meadowlark.js
+++ meadowlark/site/meadowlark.js
@@ -7,33 +7,43 @@ var handlebars = require('express3-handlebars')
   .create({ defaultLayout: 'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
 app.set('port', process.env.PORT || 3000);
-
-app.get('/', function(req, res){
-  res.render('home');
-});
-
 app.use(express.static('public'));
 
-app.get('/about', function(req, res){
-  var fortunes = [
-    "Conquer your fears or they will conquer you.", "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
-    ];
-  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', { fortune: randomFortune });
+// app.get('/', function (req, res) {
+//   res.type('text/plain');
+//   res.send('Meadowlark Travel');
+// });
+
+// app.get('/about', function (req, res) {
+//   var fortunes = [
+//     "Conquer your fears or they will conquer you.", "Rivers need springs.",
+//     "Do not fear what you don't know.",
+//     "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
+//   ];
+//   var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+//   res.type('text/plain');
+//   res.send('about', { fortune: randomFortune });
+// });
+
+app.get('/', function (req, res) {
+  res.type('text/plain');
+  res.send('Meadowlark Travel');
+});
+app.get('/about', function (req, res) {
+  res.type('text/plain');
+  res.send('About Meadowlark Travel');
 });
 
+
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   res.status(404);
   res.render('404');
 });
- 
+
 // 500 error handler (middleware)
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500);
   res.render('500');
@@ -41,7 +51,7 @@ app.use(function(err, req, res, next){
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
   console.log('hey meadowlark')
   console.log('Express started on http://localhost: ' + app.get('port') + '; press Ctrl-C to terminate.')
 });
